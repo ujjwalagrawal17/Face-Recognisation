98a25e3513bfd31d52f4096609709f7b25c84358
ujjwalagrawal17 <ujjwal.iitism@gmail.com>
2017-03-21 19:25:28 +0530

    Face Detection Added

---
+++ prashant/capture.py
@@ -0,0 +1,14 @@
+import pygame.camera
+import time
+import pygame.image
+pygame.camera.init()
+cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+cam.start()
+for i in range (20):
+    img = cam.get_image()
+    pygame.image.save(img, "prashant_" + str(i) +".jpg")
+    time.sleep(1)
+pygame.camera.quit()
+
+
+
